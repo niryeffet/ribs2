bb6c3207be7a00b82fd6f5d245d1173c6a27193d
Carlo Marcelo Arenas Belon <carenas@gmail.com>
2017-09-08 16:00:34 -0700

    fix compile error when zlib1g-dev is not available

--- tests/src/test_ribs.c
+++ tests/src/test_ribs.c
@@ -1,12 +1,16 @@
 #include "minunit.h"
 #include "test_kmeans.h"
 #include "test_ds_var_field.h"
+#ifdef HAVE_ZLIB
 #include "test_zlib.h"
+#endif
 
 static const char *all_tests() {
     mu_run_test(test_kmeans);
     mu_run_test(test_ds_var_field);
+#ifdef HAVE_ZLIB
     mu_run_test(test_zlib_vmbuf);
+#endif
     return 0;
 }
 
